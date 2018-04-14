3574e4da1176bef1408e9d121d4bf1b0f4effcc0
Jerrypoi <jerry.poi.sz@gmail.com>
2018-04-14 20:44:13 +0800

    bug fix

--- SBPNN/SBPNN.hpp
+++ SBPNN/SBPNN.hpp
@@ -13,7 +13,48 @@
 #include <fstream>
 #include <ctime>
 #include <cmath>
-struct SBPNN;
+struct SBPNN {
+    int input_n;                  // 输入层的神经元个数
+    int hidden_n;                 // 隐含层的神经元个数
+    int output_n;                 // 输出层的神经元个数
+    double *input_units;          // 输入层神经元的输出(input_units[i], i=1,2,...,input_n),其中input_units[0]=1.0
+    double *hidden_units;         // 隐藏层神经元的输出(hidden_units[i], i=1,2,...,hidden_n),其中hidden_units[0]=1.0
+    double *output_units;         // 输出层神经元的输出(output_units[i], i=1,2,...,output_n)
+    double *hidden_delta;        // 隐藏层神经元的学习误差(hidden_delta[i],i=1,...,hidden_n)
+    double *output_delta;         // 输出层神经元的学习误差(output_delta[i],i=1,...,output_n)
+    double *target;               // 目标向量(target[i],i=1,...,output_n)
+    double **input_weights;
+    // 输入层到隐藏层的连接加权系数 input_weights[i][j]表示第i个输入层与第j个隐藏层神经元的加权系数,input_weights[0][j]表示隐藏层第j个神经元的阈值
+    double **hidden_weights;
+    // 隐藏层到输出层的连接加权系数 hidden_weights[i][j]表示第i个隐藏层与第j个输出层神经元的加权系数,hidden_weights[0][j]表示输出层第j个神经元的阈值
+    // 下面两个在迭代时使用
+    double **input_prev_weights;  // 前次输入层到隐藏层权值的改变
+    double **hidden_prev_weights; // 前次隐藏层到输出层权值的改变
+    double eta;                   // 学习速率,初始值0.3, hidden_weights = hidden_prev_weights + eta*output_delta*hidden_units + momentum*hidden_prev_weights
+    double momentum;              // 动量系数,初始值0.3, input_weights = input_prev_weights + eta*hidden_delta*input_units + momentum*input_prev_weights
+    
+    //    SBPNN() {
+    //        input_units = new double[input_n];
+    //        hidden_units = new double[hidden_n];
+    //        output_units = new double[output_n];
+    //        hidden_delta = new double[hidden_n];
+    //        output_delta = new double[output_n];
+    //        input_weights = new double*[input_n + 1];
+    //        for(int i = 0;i < input_n + 1;i++) {
+    //            input_weights[i] = new double[hidden_n];
+    //        }
+    //
+    //        //TODO: Initilize the weights here
+    //
+    //        hidden_weights = new double*[hidden_n + 1];
+    //        for(int i = 0;i < input_n;i++) {
+    //            hidden_weights[i] = new double[output_n];
+    //        }
+    //        //TODO: initilize input_prev_weights and hidden..
+    //        eta = 0.3;
+    //        momentum = 0.3;
+    //    }
+};
 double fRand(double fMin, double fMax);
 void initSeed(int seed);     // 设置随机数生成器种子
 void readBPNN(SBPNN *, char* filename);    // 读取保存的网络配置及加权系数
